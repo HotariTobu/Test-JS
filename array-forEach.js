33a49227d8a27454f9f46cddf1632eb0869d2124
hotari24tools <hotari24tools@gmail.com>
2022-09-11 09:36:09 +0900

    first commit

---
+++ array-forEach.js
@@ -0,0 +1,16 @@
+[
+    'A', 'B', 
+].forEach(e => {
+    console.log(e);
+});
+
+// const a = ['A','B']
+// a.forEach(e => {
+//     console.log(e);
+// });
+
+// [
+//     'C', 'D', 
+// ].forEach(e => {
+//     console.log(e);
+// });
